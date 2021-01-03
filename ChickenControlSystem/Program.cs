07520b1cefd448ce048cb52f67d9d1d16022c02c
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-01-03 20:49:27 +0000

    Add project files.

---
+++ ChickenControlSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChickenControlSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
