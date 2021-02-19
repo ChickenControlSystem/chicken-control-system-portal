07b230dcf78f73ef3bc83cf6ed2e1e1c26548c42
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-02-19 01:01:19 +0000

    created close door command

---
+++ BLL.HardwareModules.Common/Command/ValidateOperationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BLL.HardwareModules.Common.Contract;
+using BLL.HardwareModules.Common.Sequence;
+using HAL.Operations.Enum;
+
+namespace BLL.HardwareModules.Common.Command
+{
+    public class ValidateOperationService : IValidateOperationService
+    {
+        public SequenceResultEnum GetSequenceResult(OperationResultEnum operationResult)
+        {
+            return operationResult switch
+            {
+                OperationResultEnum.Failiure => SequenceResultEnum.Fail,
+                OperationResultEnum.Succeess => SequenceResultEnum.Success,
+                _ => throw new ArgumentException()
+            };
+        }
+    }
+}
---
+++ BLL.HardwareModules.Door/Commands/CloseDoorCommand.cs
@@ -0,0 +1,40 @@
+﻿using BLL.HardwareModules.Common.Contract;
+using BLL.HardwareModules.Common.Sequence;
+using HAL.Models.Contract;
+using HAL.Models.Device;
+using HAL.Operations.Contract;
+
+namespace BLL.HardwareModules.Door.Commands
+{
+    public class CloseDoorCommand : ICommand
+    {
+        private readonly IAxisOperations _axisOperations;
+        private readonly IValidateOperationService _validateOperationService;
+        private readonly IDevice _door;
+        private readonly IDevice _floor;
+
+        public CloseDoorCommand(IAxisOperations axisOperations, IValidateOperationService validateOperationService)
+        {
+            _axisOperations = axisOperations;
+            _validateOperationService = validateOperationService;
+            _door = new DoorAxis();
+            _floor = new FloorDigitalSensor();
+        }
+
+        public SequenceResultEnum Run()
+        {
+            var result = _axisOperations
+                .MoveAxisSearch(
+                    _door,
+                    _floor,
+                    false
+                );
+            return _validateOperationService.GetSequenceResult(result);
+        }
+
+        public void HandleFail()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
