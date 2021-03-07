5035157387beb6b5a9a7de48fa1c6932244a2538
aidangannon <63330150+aidangannon@users.noreply.github.com>
2021-03-07 21:23:08 +0000

    refactored namespaces

--- HAL.ControlLine.Tests.Integration/ControlLineSockets/SendOperationTests/Given_ControlLine_SendOperation_Was_Called.cs
+++ HAL.ControlLine.Tests.Integration/ControlLineSockets/SendOperationTests/Given_ControlLine_SendOperation_Was_Called.cs
@@ -2,15 +2,15 @@
 using Crosscutting.Sockets.Client;
 using Crosscutting.Threading;
 using Crosscutting.UnitTest;
-using ControlLineTcpSockets = HAL.ControlLine.Sockets.ControlLineSockets;
 
 namespace HAL.ControlLine.Tests.Integration.ControlLineSockets.SendOperationTests
 {
-    public abstract class Given_ControlLine_SendOperation_Was_Called : GenericGivenWhenThenTests<ControlLineTcpSockets>
+    public abstract class
+        Given_ControlLine_SendOperation_Was_Called : GenericGivenWhenThenTests<Sockets.ControlLineSockets>
     {
         public override void Given()
         {
-            SUT = new ControlLineTcpSockets(
+            SUT = new Sockets.ControlLineSockets(
                 new TcpClient(
                     ConfigurationLoader.GetTestConfigurationLoader().GetControlLineSettings().GetEndPoint(),
                     8,
